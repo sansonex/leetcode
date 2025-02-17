261c59c556964f439c956f9b215b5ca601494025
Matheus <matheus.sansone@g.globo>
2025-02-17 20:38:21 -0300

    Add initial MSTest setup and example test for RemoveElement method

---
+++ LeetCode/Challenges/_27.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges;
+
+[TestClass]
+public class _27
+{
+    [TestMethod]
+    [DataRow(new[] {3, 2, 2, 3}, 3, 2)]
+    public void Main(int[] nums, int val, int expected)
+    {
+        // Act
+        var result = RemoveElement(nums, val);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    public int RemoveElement(int[] nums, int val)
+    {
+        return 1;
+    }
+}
