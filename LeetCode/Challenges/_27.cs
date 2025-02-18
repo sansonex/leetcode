e38f51aa065826b8c5869b0c7f2743938cc099f1
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-02-17 21:31:32 -0300

    Implement RemoveElement method and add test case for edge scenario

--- LeetCode/Challenges/_27.cs
+++ LeetCode/Challenges/_27.cs
@@ -7,6 +7,7 @@ public class _27
 {
     [TestMethod]
     [DataRow(new[] {3, 2, 2, 3}, 3, 2)]
+    [DataRow(new[] {0, 1, 2, 2, 3, 0, 4, 2}, 2, 5)]
     public void Main(int[] nums, int val, int expected)
     {
         // Act
@@ -18,6 +19,18 @@ public class _27
 
     public int RemoveElement(int[] nums, int val)
     {
-        return 1;
+        var i = 0;
+        for (var index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] != val)
+            {
+                var temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                i++;
+            }
+        }
+
+        return i;
     }
 }
