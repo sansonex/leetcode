0b58f39c35b2c1d02d2e9c5333cbb598b457693a
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-02-19 21:55:42 -0300

    Add MajorityElement method and unit tests for various scenarios

---
+++ LeetCode/Challenges/_169.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges;
+
+[TestClass]
+public class _169
+{
+    [TestMethod]
+    [DataRow(new[] {2, 2, 1, 1, 1, 2, 2}, 2)]
+    [DataRow(new[] {3, 2, 3}, 3)]
+    public void Main(int[] nums, int expected)
+    {
+        var response = MajorityElement(nums);
+
+        Assert.AreEqual(expected, response);
+    }
+
+    public int MajorityElement(int[] nums)
+    {
+        var counter = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (counter.ContainsKey(nums[i]))
+            {
+                counter[nums[i]] += 1;
+                if (counter[nums[i]] > nums.Length / 2)
+                {
+                    return nums[i];
+                }
+            }
+            else
+            {
+                counter[nums[i]] = 1;
+            }
+        }
+
+        return counter.First(x => x.Value > nums.Length / 2).Key;
+    }
+}
