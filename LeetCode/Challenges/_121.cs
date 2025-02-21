196149ab2243d4d6d1bb2bc5dec0a948f6031b53
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-02-21 01:59:21 -0300

    Add MaxProfit method and unit tests for various scenarios

---
+++ LeetCode/Challenges/_121.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges;
+
+[TestClass]
+public class _121
+{
+    [TestMethod]
+    [DataRow(new[] {7, 1, 5, 3, 6, 4}, 5)]
+    [DataRow(new[] {2, 1, 4}, 3)]
+    public void Main(int[] nums, int k)
+    {
+        Assert.AreEqual(k, MaxProfit(nums));
+    }
+
+    public int MaxProfit(int[] prices)
+    {
+        var left = 0;
+        var right = 0;
+        var maxProfit = 0;
+
+        while (right <= prices.Length - 2)
+        {
+            right++;
+            var profit = prices[right] - prices[left];
+            if (profit > maxProfit)
+            {
+                maxProfit = profit;
+            }
+
+            if (prices[right] < prices[left])
+            {
+                left = right;
+            }
+        }
+
+        return maxProfit;
+    }
+}
