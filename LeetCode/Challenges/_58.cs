f55789759c0aafe34e5bb48e3e4ab120d6e419fe
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-02-25 00:30:11 -0300

    Add LengthOfLastWord method and unit tests for various input cases

---
+++ LeetCode/Challenges/_58.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges
+{
+    [TestClass]
+    public class _58
+    {
+        [TestMethod]
+        [DataRow("Hello World", 5)]
+        [DataRow("   fly me   to   the moon  ", 4)]
+        [DataRow("luffy is still joyboy", 6)]
+        public void Main(string s, int expected)
+        {
+            // Act
+            var result = LengthOfLastWord(s);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public int LengthOfLastWord(string s)
+        {
+            var count = 0;
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    count++;
+                    continue;
+                }
+
+                if (count > 0)
+                {
+                    return count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
