0b5fa826d31246ec959020346940edf2134f2250
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-02-27 01:11:31 -0300

    Add StrStr method and unit tests for substring search

---
+++ LeetCode/Challenges/Easy/_28.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _28 
+    {
+        [TestMethod]
+        [DataRow("sadbutsad", "sad", 0)]
+        [DataRow("leetcode", "leeto", -1)]
+        public void Main(string haystack, string needle, int expected)
+        {
+            Assert.AreEqual(StrStr(haystack, needle), expected);
+        }
+
+        public int StrStr(string haystack, string needle)
+        {
+            var left = 0;
+            var right = 0;
+            var p = 0;
+
+            while (right < haystack.Length)
+            {
+                if (haystack[right] == needle[p])
+                {
+                    right++;
+                    p++;
+                    if (p == needle.Length)
+                    {
+                        return left;
+                    }
+                }
+                else
+                {
+                    left++;
+                    right = left;
+                    p = 0;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
