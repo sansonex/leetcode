0f6e755edf39d7cb31f457f5c70c01f10be4e140
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-01 16:33:33 -0300

    Add BuildArray method and unit tests for array transformation

---
+++ LeetCode/Challenges/Easy/_1920.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _1920
+    {
+        [TestMethod]
+        [DataRow(new[] {0, 2, 1, 5, 3, 4}, new[] {0, 1, 2, 4, 5, 3})]
+        [DataRow(new[] {5,0,1,2,3,4}, new[] {4,5,0,1,2,3})]
+        public void Main(int[] nums, int[] expected)
+        {
+            var response = BuildArray(nums);
+            for (int i = 0; i < response.Length; i++)
+            {
+                Assert.AreEqual(response[i], expected[i]);
+            }
+        }
+
+        public int[] BuildArray(int[] nums)
+        {
+            var copy = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                copy[i] = nums[nums[i]];
+            }
+
+            return copy;
+        }
+    }
+}
