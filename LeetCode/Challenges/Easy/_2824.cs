1d30928f7b62aeca14aab759ac28addd9a9b4591
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-05 23:09:52 -0300

    Add CountPairs method and unit tests for pair counting under target

---
+++ LeetCode/Challenges/Easy/_2824.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _2824
+    {
+        [TestMethod]
+        [DataRow(new[] {-1, 1, 2, 3, 1}, 2, 3)]
+        [DataRow(new[] {-6, 2, 5, -2, -7, -1, 3}, -2, 10)]
+        public void Main(int[] nums, int target, int expected)
+        {
+            Assert.AreEqual(expected, CountPairs(nums, target));
+        }
+
+        public int CountPairs(IList<int> nums, int target)
+        {
+            var s = 0;
+            for (int x = 0; x < nums.Count; x++)
+            {
+                for (int i = x + 1; i < nums.Count; i++)
+                {
+                    if (nums[x] + nums[i] < target)
+                        s++;
+                }
+            }
+
+            return s;
+        }
+    }
+}
