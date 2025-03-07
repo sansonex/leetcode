b1234876ae0ca74f9c067b8df1d2d7d4aaceb5b3
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-07 00:53:09 -0300

    Add IsPalindrome method and unit tests for palindrome validation

---
+++ LeetCode/Challenges/Easy/_125.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _125
+    {
+        [TestMethod]
+        [DataRow("A man, a plan, a canal: Panama", true)]
+        [DataRow("race a car", false)]
+        public void Main(string nums, bool expected)
+        {
+            Assert.AreEqual(expected, IsPalindrome(nums));
+        }
+
+        public bool IsPalindrome(string s)
+        {
+            var l = 0;
+            var r = s.Length - 1;
+
+            while (l < r)
+            {
+                if (!char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                    continue;
+                }
+
+                if (!char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                    continue;
+                }
+
+                if (char.ToLowerInvariant(s[l]) == char.ToLowerInvariant(s[r]))
+                {
+                    l++;
+                    r--;
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
