900de3b447db4f14d26afd6a62a00d2c33dfb824
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-09 04:04:21 -0300

    Add IsSubsequence method and unit tests for subsequence validation

---
+++ LeetCode/Challenges/Easy/_392.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _392
+    {
+        [TestMethod]
+        [DataRow("abc", "ahbgdc", true)]
+        [DataRow("axc", "ahbgdc", false)]
+        [DataRow("b", "c", false)]
+        [DataRow("acb", "ahbgdc", false)]
+        public void Main(string s, string t, bool expected)
+        {
+            var response = IsSubsequence(s, t);
+
+            Assert.AreEqual(expected, response);
+        }
+
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0)
+                return true;
+
+            var c = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[c])
+                    c++;
+
+                if (c == s.Length && c > 0)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
