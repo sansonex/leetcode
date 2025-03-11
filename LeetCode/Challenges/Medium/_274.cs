a4f7c8a34b601c9b0b49da9d598213a10a578bf5
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-11 01:55:46 -0300

    Add HIndex method and unit tests for H-index calculation

---
+++ LeetCode/Challenges/Medium/_274.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Medium
+{
+    [TestClass]
+    public class _274
+    {
+        [TestMethod]
+        [DataRow(new[] {3, 0, 6, 1, 5}, 3)]
+        [DataRow(new[] {1, 3, 1}, 1)]
+        [DataRow(new[] {100}, 1)]
+        [DataRow(new[] {0, 0, 2}, 1)]
+        public void Main(int[] nums, int expected)
+        {
+            // Arrange & Act & Assert
+            Assert.AreEqual(expected, HIndex(nums));
+        }
+
+        public int HIndex(int[] citations)
+        {
+            var h = 0;
+            Array.Sort(citations);
+            Array.Reverse(citations);
+
+            for (var i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] >= i + 1)
+                {
+                    h = i + 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return h;
+        }
+    }
+}
