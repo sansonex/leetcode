f66df7675fc43d4d20399e55eb11d4212589e24f
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-12 01:22:48 -0300

    Add ReverseWords method and unit tests for reversing words in a string

---
+++ LeetCode/Challenges/Medium/_151.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Medium
+{
+    [TestClass]
+    public class _151
+    {
+        [TestMethod]
+        [DataRow("the sky", "sky the")]
+        [DataRow("  hello world  ", "world hello")]
+        public void Main(string str, string expected)
+        {
+            // Arrange & Act & Assert
+            Assert.AreEqual(expected, ReverseWords(str));
+        }
+
+        public string ReverseWords(string s)
+        {
+            var left = 0;
+            var right = 0;
+            var words = new List<string>();
+
+            while (right < s.Length)
+            {
+                if (s[right] == ' ' || right == s.Length - 1)
+                {
+                    var word = s.Substring(left, right - left + 1).Trim();
+                    if (word != "")
+                    {
+                        words.Add(word);
+                        left = right;
+                    }
+                }
+
+                right++;
+            }
+
+            words.Reverse();
+            return string.Join(" ", words);
+        }
+    }
+}
