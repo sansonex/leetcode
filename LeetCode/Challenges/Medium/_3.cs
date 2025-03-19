7ad924c6b4c51753204e8ca0dd291a764908b3f0
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-19 00:50:03 -0300

    Add LengthOfLongestSubstring method and unit tests for longest substring without repeating characters

---
+++ LeetCode/Challenges/Medium/_3.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Medium
+{
+    [TestClass]
+    public class _3
+    {
+        [TestMethod]
+        [DataRow("abcabcbb", 3)]
+        [DataRow("bbbbb", 1)]
+        [DataRow("pwwkew", 3)]
+        [DataRow("dvdf", 3)]
+        public void Main(string input, int expected)
+        {
+            Assert.AreEqual(expected, LengthOfLongestSubstring(input));
+        }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            var dic = new HashSet<char>();
+            var left = 0;
+            var right = 0;
+            var max = 0;
+
+            while (right < s.Length)
+            {
+                if (dic.Add(s[right]))
+                {
+                    max = Math.Max(max, right - left + 1);
+                }
+                else
+                {
+                    while (!dic.Add(s[right]))
+                    {
+                        dic.Remove(s[left]);
+                        left++;
+                    }
+                }
+
+                right++;
+            }
+
+            return max;
+        }
+    }
+}
