21185235fb94ee64c1442dc6e46fea4caa0ce777
Matheus Sansone <22122007+sansonex@users.noreply.github.com>
2025-03-26 00:21:11 -0300

    Add MiddleNode method and unit tests for finding the middle of a linked list

---
+++ LeetCode/Challenges/Easy/_876.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Challenges.Easy
+{
+    [TestClass]
+    public class _876
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(Data1))]
+        public void Main(ListNode input, ListNode expected)
+        {
+            var response = MiddleNode(input);
+            Assert.IsTrue(CompareListNode(expected, response));
+        }
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            var ahead = head;
+            while (ahead?.next != null)
+            {
+                head = head.next;
+                ahead = ahead.next.next;
+            }
+
+            return head;
+        }
+
+        private bool CompareListNode(ListNode expected, ListNode response)
+        {
+            while (expected != null && response != null)
+            {
+                if (expected.val != response.val)
+                {
+                    return false;
+                }
+
+                expected = expected.next;
+                response = response.next;
+            }
+
+            return expected == null && response == null;
+        }
+
+        private static IEnumerable<object[]> Data1 =>
+        [
+            [new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), new ListNode(3, new ListNode(4, new ListNode(5)))]
+        ];
+    }
+}
